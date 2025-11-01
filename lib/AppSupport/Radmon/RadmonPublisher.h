dc563b85c22ae7d7140ceadff929199ab1c8f107
André Fiedler <SunboX@users.noreply.github.com>
2025-11-01 22:47:54 +0100

    Adds Radmon.org publishing support
    Implements support for publishing radiation data to Radmon.org.
    
    This includes:
    - Adds RadmonPublisher class to handle data submission.
    - Adds configuration parameters for Radmon.org credentials to the web portal.
    - Adds Radmon settings to the AppConfig.
    
    The RadmonPublisher sends tube rate and dose rate to radmon.org
    using user-provided credentials. Data is submitted via HTTP GET requests.

---
+++ lib/AppSupport/Radmon/RadmonPublisher.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <Arduino.h>
+#include <WiFiClient.h>
+#include "AppConfig/AppConfig.h"
+#include "DeviceManager.h"
+
+class RadmonPublisher
+{
+public:
+    RadmonPublisher(AppConfig &config, Print &log);
+
+    void begin();
+    void updateConfig();
+    void loop();
+    void onCommandResult(DeviceManager::CommandType type, const String &value);
+
+private:
+    bool isEnabled() const;
+    bool publishPending();
+    bool sendRequest(const String &query);
+    static String urlEncode(const String &input);
+
+    AppConfig &config_;
+    Print &log_;
+    String pendingCpm_;
+    String pendingUsv_;
+    bool haveCpm_ = false;
+    bool haveUsv_ = false;
+    bool publishQueued_ = false;
+    unsigned long lastAttemptMs_ = 0;
+    unsigned long suppressUntilMs_ = 0;
+};
